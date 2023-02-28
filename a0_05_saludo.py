0fcc5179c142d6d1089bf363a62b55b9e025d699
pitito952 <agl952@protonmail.ch>
2023-02-28 17:36:29 -0400

    Se muestra mensaje en base al dato ingresado en pantalla.

--- a0_05_saludo.py
+++ a0_05_saludo.py
@@ -2,16 +2,19 @@ import flet
 from flet import Page, Row, TextField, ElevatedButton, Column, Text
 
 
+
+
 def main(page: Page):
     
-    
+    txt_nombre = TextField(label="Escribe tu nombre:")
+
     def botonPulsado(evento):
-        print("Botón pulsado")
+        print(f"Hola....{txt_nombre.value}")
 
     # Agregar controles a la página sin usar una variable
     page.add(
         Row(controls=[
-            TextField(label="Escribe tu nombre:"),
+            txt_nombre,
             ElevatedButton(text="Obtener nombre!", on_click=botonPulsado)
             ])
     )
