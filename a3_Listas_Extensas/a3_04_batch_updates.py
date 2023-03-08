ce830828b5bd8e9cfd8523becc31f6bb7f63ca52
pitito952 <agl952@protonmail.ch>
2023-03-08 19:27:53 -0400

    Batch update - Listview

---
+++ a3_Listas_Extensas/a3_04_batch_updates.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    lista = ft.ListView(expand=1, spacing=10, item_extent=50)
+    page.add(lista)
+
+    for i in range(5100):
+        lista.controls.append(ft.Text(f"Linea {i}"))
+
+        if i % 500 == 0:
+            page.update()
+
+    page.update()
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)    
