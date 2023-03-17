6c8703af89075d106a07c666d7645917e4e2e4f6
pitito952 <agl952@protonmail.ch>
2023-03-17 18:29:33 -0400

    Ejemplo de Animación

---
+++ a7_Animaciones/a7_02_Rotacion.py
@@ -0,0 +1,33 @@
+
+from math import pi
+import flet as ft
+
+
+def main(page: ft.Page):
+
+    contenedor = ft.Container(
+        width=100,
+        height=70,
+        bgcolor="blue",
+        border_radius=5,
+        rotate = ft.transform.Rotate(0, alignment=ft.alignment.center), 
+        animate_rotation=ft.animation.Animation(300, ft.AnimationCurve.BOUNCE_OUT)
+    )
+
+    def animate(evento):
+        contenedor.rotate.angle += pi / 2
+        page.update()
+
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.spacing = 30
+
+    page.add(
+        contenedor,
+        ft.ElevatedButton("Animar!", on_click=animate)
+    )
+
+
+ft.app(target=main)    
+
